9f934812a3b446ae89a29026ad48d7a94d79acb8
cdiaz19 <cdiaz@pernix-solutions.com>
2015-11-10 14:05:06 -0600

    US48- Design Initial Page

---
+++ www/js/mainController/mainController.js
@@ -0,0 +1,19 @@
+(function(){
+    var app = angular.module('starter', ['ionic'])
+	
+	app.controller('smartLogo', function($scope){
+      $scope.frontendlogo = "smart_quote_logo.png";
+    });
+
+    app.controller('backgroundInitial', function($scope){
+      $scope.frontendbackground = "background.png";
+    });
+
+    app.controller('button', function($scope, $routeParams, $location){
+      $scope.buttonname = "Create Account";
+      $scope.go = function(){
+        $window.location.href = ' / ';
+      }
+    });
+ 
+}());
